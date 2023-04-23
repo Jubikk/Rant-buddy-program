b34b704fe32bae6f44beba390ce5a1385b63a83f
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-04-24 02:10:41 +0800

    2nd

--- Rant Buddy program.py
+++ Rant Buddy program.py
@@ -1,2 +1,14 @@
 #Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.4 Exercise 3
+filename = "mylife.txt"
+
+with open(filename, "w") as file:
+    while True:
+        line = input("Hi, how are you? Wanna tell me something? ")
+        file.write(line + "\n")
+        another_lines = input("Wanna tell me more? y/n? ")
+        if another_lines.lower() != "y":
+            break
+
+print("TEXT WAS SAVED IN", filename)
+
 
